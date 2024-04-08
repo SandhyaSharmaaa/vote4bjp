d24fb58b7d1b95724a25e5730c2a25a011f527b8
sandhya <sandhyasharmavdoit@gmail.com>
2024-04-08 22:23:12 +0530

    fix: ui

--- src/pages/Blog.jsx
+++ src/pages/Blog.jsx
@@ -1,14 +1,20 @@
 import videoFile from "../assets/Finals.mp4"
+import logo from "../assets/logo.png"
 const App = () => {
   
   return (
-    <div style={{ background: 'linear-gradient(135deg, #f58e34, #3eba38)', height: '100vh', display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
-      <video width="750" height="500" autoPlay loop controls>
+    <main className="gradient-background" >
+      <video width="750" height="500" autoPlay muted loop controls >
         <source src={videoFile} type="video/mp4" />
+        
         
         Your browser does not support the video tag.
       </video>
-    </div>
+      <div className="bottom-bar">
+        <div></div>
+        <img src={logo} />
+      </div>
+    </main>
   );
 };
 
