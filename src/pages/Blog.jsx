e4b421f86eedeb31d33abd429d8166d7dc7a46e1
sandhya <sandhyasharmavdoit@gmail.com>
2024-04-08 22:51:12 +0530

    fix: ui

--- src/pages/Blog.jsx
+++ src/pages/Blog.jsx
@@ -4,12 +4,14 @@ const App = () => {
   
   return (
     <main className="gradient-background" >
+      <div className="video-div">
       <video width="750" height="500" autoPlay muted loop controls >
         <source src={videoFile} type="video/mp4" />
         
         
         Your browser does not support the video tag.
       </video>
+      </div>
       <div className="bottom-bar">
         <div className="text">आइए, 4 अप्रैल को हमारे यशश्वी प्रधान मंत्री श्री नरेंद्र मोदी जी का हार्दिक स्वागत करें, और हम सब जमोई के पावन धरती पर लाखों की तादात में एकत्रित होकर उनके उज्वल भारत के सपनों को पूरा करने में उनका साथ दें। - सम्राट चौधरी</div>
         <img src={logo} />
