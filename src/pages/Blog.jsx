86b5a12722374be4df6c5c5a973bd7fc626ea669
sandhya <sandhyasharmavdoit@gmail.com>
2024-04-08 21:15:40 +0530

    shdb

--- src/pages/Blog.jsx
+++ src/pages/Blog.jsx
@@ -2,7 +2,7 @@ const App = () => {
   return (
     <div style={{ background: 'linear-gradient(135deg, #f58e34, #3eba38)', height: '100vh', display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
       <video width="750" height="500" autoPlay loop controls>
-        <source src="Final.mp4" type="video/mp4" />
+        <source src="Finals.mp4" type="video/mp4" />
         Your browser does not support the video tag.
       </video>
     </div>
