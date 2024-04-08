246e694cbb6c14b4ef848e6d88353dc8ba6d3aab
sandhya <sandhyasharmavdoit@gmail.com>
2024-04-08 21:26:49 +0530

    video

--- src/pages/Blog.jsx
+++ src/pages/Blog.jsx
@@ -1,8 +1,11 @@
+import videoFile from "../assets/Finals.mp4"
 const App = () => {
+  
   return (
     <div style={{ background: 'linear-gradient(135deg, #f58e34, #3eba38)', height: '100vh', display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
       <video width="750" height="500" autoPlay loop controls>
-        <source src="Finals.mp4" type="video/mp4" />
+        <source src={videoFile} type="video/mp4" />
+        
         Your browser does not support the video tag.
       </video>
     </div>
