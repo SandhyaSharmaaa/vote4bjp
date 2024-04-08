2168e57d906bf5fcc9a69a9e929a81a9c0b5196e
sandhya <sandhyasharmavdoit@gmail.com>
2024-04-08 21:04:53 +0530

    bjp

---
+++ src/pages/Blog.jsx
@@ -0,0 +1,12 @@
+const App = () => {
+  return (
+    <div style={{ background: 'linear-gradient(135deg, #f58e34, #3eba38)', height: '100vh', display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
+      <video width="750" height="500" autoPlay loop controls>
+        <source src="Final.mp4" type="video/mp4" />
+        Your browser does not support the video tag.
+      </video>
+    </div>
+  );
+};
+
+export default App;
